6b3238ffe78a6cad7fc095e6877c0c2ae97c7645
kaerez <kaerez@users.noreply.github.com>
2024-07-03 09:02:25 +0300

    Create permacc.js

---
+++ Perma.cc/permacc.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Perma.cc
+// @namespace    https://github.com/kaerez/jsmonkey-pub
+// @version      1.0
+// @description  Save to Perma.cc
+// @author       EK
+// @match        *://*/*
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// @run-at       document-start
+// @icon         https://github.com/kaerez/jsmonkey-pub/Perma.cc/static/icon.png
+// @noframes
+// @supportURL   https://github.com/kaerez/jsmonkey-pub/Perma.cc
+// @downloadURL  https://github.com/kaerez/jsmonkey-pub/Perma.cc/permacc.js
+// @updateURL    https://github.com/kaerez/jsmonkey-pub/Perma.cc/permacc.js
+// ==/UserScript==
+
+
+function savepermacc(){
+    GM_openInTab('https://perma.cc/service/bookmarklet-create/?v=1&url='+encodeURIComponent(location.href),true);
+}
+
+function downpermacc(){
+    var currentUrl = window.location.href;
+    var urlPattern = /^https?:\/\/(www\.)?perma\.cc\/([^?&/\\]*)/i;
+    var matches = currentUrl.match(urlPattern);
+
+    if (!matches || matches.length < 3) {
+        return; // Stop and do nothing if no valid fragment is found
+    }
+    GM_openInTab(currentUrl+'?type=warc_download',true);
+}
+
+if (location.href.startsWith("https://perma.cc/")) {
+    GM.registerMenuCommand("Download WARC Archive", downpermacc);
+}
+else {
+    GM.registerMenuCommand("Save", savepermacc);
+}
