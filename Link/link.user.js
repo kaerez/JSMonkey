829c4b079295ac0b8480a222873d786b6aed931a
kaerez <kaerez@users.noreply.github.com>
2024-07-07 11:32:20 +0300

    Update link.user.js

--- Link/link.user.js
+++ Link/link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Link It
 // @namespace    https://github.com/kaerez/JSMonkey
-// @version      1.1
+// @version      1.2
 // @description  Insert link at the top of a page
 // @author       EK
 // @match        *://*/*
@@ -27,14 +27,11 @@ function setdata(){
 	GM_setValue("Target", prompt("Pointing to where?"))
 }
 
-function linkit(){
+if (location.href.startsWith(URI)) {
 	var newHTML = document.createElement ('div');
 	newHTML.innerHTML = '<br><div style="background-color:red;font-size:1000%;text-align:center;"><a href='+tgt+'>'+name+'</a></div><br>';
 	//document.body.appendChild (newHTML);
-	document.body.insertBefore (newHTML, document.body.firstChild);
-};
-
-if (location.href.startsWith(URI)) {
-    linkit();
+	document.body.insertBefore (newHTML, document.body.firstChild);}
 }
+console.log("location.href.startsWith(URI): "+location.href.startsWith(URI))
 GM.registerMenuCommand("Reset and configure", setdata);
